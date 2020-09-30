2e307773f5310724c39f70cd085c2df920a62c19
dan patrick <dan@solliance.net>
2020-09-30 10:59:19 -0400

    add rgs

---
+++ azure/templates/rg/301/modules/rg/main.tf
@@ -0,0 +1,22 @@
+# #################################################################################
+# Build5Nines: Terraform Kung-fu
+# https://build5nines.com/teraform-kung-fu
+# Copyright (c) 2020 Build5Nines.com
+# MIT License - https://github.com/Build5Nines/terraform-kung-fu/blob/master/LICENSE
+# WARNING: These scripts could either cause resume generating events or get you promoted.
+# Please, proceed with extreme caution!
+# #################################################################################
+# PURPOSE:
+# - Create multiple resource groups using a list in the locals
+# #################################################################################
+# USAGE:
+# - Update the local variables to configure the resource
+# #################################################################################
+# HELPFUL RESOURCES:
+# - <add-links>
+# #################################################################################
+
+locals {
+  rg_prefix = "b59s"
+}
+
---
+++ azure/templates/rg/301/modules/rg/rg.tf
@@ -0,0 +1,24 @@
+# #################################################################################
+# Build5Nines: Terraform Kung-fu
+# https://build5nines.com/teraform-kung-fu
+# Copyright (c) 2020 Build5Nines.com
+# MIT License - https://github.com/Build5Nines/terraform-kung-fu/blob/master/LICENSE
+# WARNING: These scripts could either cause resume generating events or get you promoted.
+# Please, proceed with extreme caution!
+# #################################################################################
+# PURPOSE:
+# - <fill in purpose>
+# #################################################################################
+# USAGE:
+# - Update the local variables to configure the resource
+# #################################################################################
+# HELPFUL RESOURCES:
+# - <add-links>
+# #################################################################################
+
+resource "azurerm_resource_group" "b59s_rg" {
+  count = length(var.locations)
+
+  location = var.locations[count.index]
+  name     = "${local.rg_prefix}-${var.locations[count.index]}-rg"
+}
