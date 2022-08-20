6803cdb1c58d8e1850252c8c0caa94612af98eb8
Chris Pietschmann <chris@build5nines.com>
2022-08-19 20:49:49 -0400

    fix typo

--- microsoft-azure/workloads/azure-express-route-with-vnet-gateway/main.tf
+++ microsoft-azure/workloads/azure-express-route-with-vnet-gateway/main.tf
@@ -4,7 +4,7 @@ locals {
 
     # define the Azure resource names
     resource_name_prefix= "E1-B59-PRD-NET" # {region}-{org}-{env}-{app}
-    resource_group_name = "${resource_name_prefix}-rg"
+    resource_group_name = "${local.resource_name_prefix}-rg"
 
 
     # set of common tags to apply to all Azure resources
