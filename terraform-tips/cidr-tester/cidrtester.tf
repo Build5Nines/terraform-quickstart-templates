4d5026f0a93ec1e1390b535d16d2f6f812ab5617
Chris Pietschmann <chris@build5nines.com>
2023-02-23 16:13:50 -0500

    Update cidrtester.tf

--- terraform-tips/cidr-tester/cidrtester.tf
+++ terraform-tips/cidr-tester/cidrtester.tf
@@ -1,16 +1,20 @@
 locals {
-  vnet_mgt_cidr = "172.16.0.0/16"
+  vnet_mgt_cidr = "10.0.1.0/24"
 }
 
 output "subnets" {
   value = [
     {
       name = "subnet1"
-      cidr = cidrsubnet(local.vnet_mgt_cidr, 8, 0)
+      cidr = cidrsubnet(local.vnet_mgt_cidr, 4, 0)
     },
     {
       name = "subnet2"
-      cidr = cidrsubnet(local.vnet_mgt_cidr, 6, 1)
+      cidr = cidrsubnet(local.vnet_mgt_cidr, 4, 1)
+    },
+    {
+      name = "subnet2"
+      cidr = cidrsubnet(local.vnet_mgt_cidr, 4, 4)
     },
   ]
 }
