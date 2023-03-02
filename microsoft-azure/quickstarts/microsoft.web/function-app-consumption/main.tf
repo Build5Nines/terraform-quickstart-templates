6322fd312887c9df0b886c47069a2d7855f33d0a
Chris Pietschmann <chris@build5nines.com>
2023-03-02 13:46:26 -0500

    Add Function App w/ Consumption plan quickstarts

---
+++ microsoft-azure/quickstarts/microsoft.web/function-app-consumption/main.tf
@@ -0,0 +1,81 @@
+# #################################################################################
+# Build5Nines: Terraform Quickstart Templates
+# https://build5nines.com/teraform-quickstart-templates
+# Copyright (c) 2023 Build5Nines.com, Chris Pietschmann
+# MIT License - https://github.com/Build5Nines/terraform-quickstart-templates/blob/master/LICENSE
+# WARNING: These scripts could either cause resume generating events or get you promoted.
+# Please, proceed with extreme caution!
+# #################################################################################
+# PURPOSE:
+# - Create an Azure Function App using Consumption plan pricing
+# #################################################################################
+# USAGE:
+# - Update the local variables to configure the resource
+# #################################################################################
+# HELPFUL RESOURCES:
+# - https://build5nines.com/get-started-with-terraform-on-microsoft-azure/
+# #################################################################################
+
+provider "azurerm" {
+  # AzureRM provider 2.x
+  version = "~>2.0"
+  # v2.x required "features" block
+  features {}
+}
+
+locals {
+    resource_group_name       = "b59_functions"
+    location                  = "eastus"
+    
+    function_app_name                 = "b59_func_app"
+    app_service_plan_name             = "b59_func_app_plan"
+    function_app_storage_account_name = "b59funcappsa"
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "primary" {
+  name     = local.resource_group_name
+  location = local.location
+}
+
+# Create Azure Storage Account required for Function App
+resource azurerm_storage_account "primary" {
+  name                     = local.function_app_storage_account_name
+  resource_group_name      = azurerm_resource_group.primary.name
+  location                 = azurerm_resource_group.primary.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Create Azure App Service Plan using Consumption pricing
+resource azurerm_app_service_plan "primary" {
+  name                = local.app_service_plan_name
+  location            = azurerm_resource_group.primary.location
+  resource_group_name = azurerm_resource_group.primary.name
+  kind                = "FunctionApp"
+  reserved            = true
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+# Create an Azure Function App
+resource azurerm_function_app "primary" {
+  name                       = local.function_app_name
+  resource_group_name        = azurerm_resource_group.primary.name
+  location                   = azurerm_resource_group.primary.location
+
+  app_service_plan_id        = azurerm_app_service_plan.primary.id
+  
+  storage_account_name       = azurerm_storage_account.primary.name
+  storage_account_access_key = azurerm_storage_account.primary.primary_access_key
+  
+  version                    = "~3"
+  
+  site_config {
+    always_on = true
+  }
+}
+
