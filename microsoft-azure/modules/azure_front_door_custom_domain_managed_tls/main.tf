5dd93840e0d51ffd0fd47a8a77839e5a1703607f
Chris Pietschmann <chris@build5nines.com>
2023-11-28 13:03:36 -0500

    add a couple Front Door modules

---
+++ microsoft-azure/modules/azure_front_door_custom_domain_managed_tls/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+variable front_door_profile_id {
+  type        = string
+  description = "The Azure Resource ID of the Azure Front Door Profile"
+}
+
+variable host_name {
+    type        = string
+    description = "The Host Name of the Custom Domain."
+}
+
+
+/*
+Microsoft.Cdn/profiles/customdomains
+*/
+resource azurerm_cdn_frontdoor_custom_domain "primary" {
+  name                     = "default-domain"
+  cdn_frontdoor_profile_id = var.front_door_profile_id
+  host_name                = var.host_name
+
+  tls {
+    # Use an Azure managed Certificate
+    certificate_type    = "ManagedCertificate"
+    minimum_tls_version = "TLS12"
+  }
+}
+
+
+output custom_domain {
+  value = azurerm_cdn_frontdoor_custom_domain.primary
+}
---
+++ microsoft-azure/modules/azure_front_door_profile/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+variable name {
+    type        = string
+    description = "The name of the Front Door."
+}
+
+variable resource_group_name {
+    type        = string
+    description = "The name of the resource group in which to create the Front Door."
+}
+
+variable sku {
+    type        = string
+    description = "The SKU for the Front Door"
+    default     = "Standard_AzureFrontDoor"
+
+    validation {
+        condition     = var.example_variable == "Standard_AzureFrontDoor" || var.example_variable == "Premium_AzureFrontDoor"
+        error_message = "SKU must be either 'Standard_AzureFrontDoor' or 'Premium_AzureFrontDoor'."
+    }
+}
+
+
+
+/*
+Microsoft.Cdn/profiles
+kind: 'frontdoor'
+*/
+resource azurerm_cdn_frontdoor_profile "primary" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  sku_name            = var.sku
+}
+
+/*
+Microsoft.Cdn/profiles/afdendpoints
+*/
+resource azurerm_cdn_frontdoor_endpoint "primary" {
+  name                     = "${var.name}-endpoint"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.primary.id
+}
+
+
+
+
+output front_door_profiile {
+    value = azurerm_cdn_frontdoor_profile.primary
+}
+
+output front_door_endpoint {
+    value = azurerm_cdn_frontdoor_endpoint.primary
+}
